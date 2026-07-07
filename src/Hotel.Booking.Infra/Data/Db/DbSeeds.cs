f036e7f75273614ddc5ae5c98ad328943e6c602d
Silas Dantas <silas-dantas@hotmail.com>
2026-07-07 09:18:48 -0300

    fix: remove duplicate room seed

--- src/Hotel.Booking.Infra/Data/Db/DbSeeds.cs
+++ src/Hotel.Booking.Infra/Data/Db/DbSeeds.cs
@@ -6,16 +6,9 @@ namespace Hotel.Booking.Infra.Data.Db
     {
         public static void SeedData(HotelDbContext dbContext)
         {
-            //if (dbContext.Bookings.Any())
-            //    dbContext.Bookings.RemoveRange(Bookings.ToList());
-
-            //if (Rooms.Any())
-            //    Rooms.RemoveRange(Rooms.ToList());
-
             if (!dbContext.Rooms.Any())
             {
                 var roomA = new RoomEntity(Guid.Parse("0b5786eb-cb60-4e89-bb4a-212d58d5efcd"), "MIO Cancún Hotel Boutique, Queen Suite");
-                dbContext.Rooms.Add(roomA);
                 dbContext.Bookings.Add(new BookingEntity(Guid.Parse("d234a714-2298-4b7d-a957-cc4c3cc28786"), DateTime.Now.AddDays(1), DateTime.Now.AddDays(4), roomA.Id, "Aegon Targaryen"));
                 dbContext.Bookings.Add(new BookingEntity(Guid.Parse("28630ed2-55a0-4b7e-bfa2-31a68502296c"), DateTime.Now.AddDays(10), DateTime.Now.AddDays(12), roomA.Id, "Jhow Snow"));
                 var roomB = new RoomEntity(Guid.NewGuid(), "Hotel Krystal Cancún, Standard King Room");
